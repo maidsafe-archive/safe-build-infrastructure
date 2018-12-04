a1d618bbb5e1ba7779b9408a3fd1c5f6a495ef9d
Chris O'Neil <chris.oneil@gmail.com>
2018-12-04 12:04:24 +0000

    feat/plugin: job DSL plugin
    This plugin gives us the ability to define our jobs in code. Note that
    pipelines should not be defined using this plugin. Just the actual *job*
    that contains the pipeline should be configured using this plugin.
    
    There's also a little utility script here that lists the plugins for the
    instance. This is so that you can get the ID and version of the plugin
    to add to the plugins.txt file.

---
+++ scripts/sh/list_jenkins_plugins.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sudo yum install -y perl
+
+echo
+echo "please provide your jenkins username"
+read username
+echo "please provide your password"
+read -s password
+
+export JENKINS_HOST=$username:$password@localhost:8080
+curl -sSL "http://$JENKINS_HOST/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins" | perl -pe 's/.*?<shortName>([\w-]+).*?<version>([^<]+)()(<\/\w+>)+/\1 \2\n/g'|sed 's/ /:/'
