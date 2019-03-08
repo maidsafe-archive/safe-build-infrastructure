d289592e73701258b1a16b3316fdbebc843ad1e1
Chris O'Neil <chris.oneil@gmail.com>
2019-03-08 10:55:22 +0000

    feat/env: open port for wireguard

--- scripts/sh/create_dev_security_group.sh
+++ scripts/sh/create_dev_security_group.sh
@@ -37,6 +37,12 @@ function open_ports() {
             --port "$port" \
             --cidr 0.0.0.0/0
     done
+    aws ec2 authorize-security-group-ingress \
+        --region eu-west-2 \
+        --group-id "$jenkins_group_id" \
+        --protocol udp \
+        --port "51820" \
+        --cidr 0.0.0.0/0
     for port in "${windows_ports[@]}"
     do
         echo "Opening port $port on windows slaves group."
