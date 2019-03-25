55b002d9bb7e38f4519c304eb77e1c6bc0d43911
Chris O'Neil <chris.oneil@gmail.com>
2019-03-25 13:06:05 +0000

    feat/env: open port 8080 for Windows slave group
    This also re-adds the Windows slave definition to the Jenkins
    configuration. It was taken out while the Windows part wasn't working
    yet.

--- terraform/prod/security_groups.tf
+++ terraform/prod/security_groups.tf
@@ -294,6 +294,15 @@ resource "aws_security_group_rule" "windows_slaves_egress_http" {
   security_group_id = "${aws_security_group.windows_slaves.id}"
 }
 
+resource "aws_security_group_rule" "windows_slaves_egress_8080" {
+  type = "egress"
+  from_port = 8080
+  to_port = 8080
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.windows_slaves.id}"
+}
+
 resource "aws_security_group_rule" "windows_slaves_egress_https" {
   type = "egress"
   from_port = 443
