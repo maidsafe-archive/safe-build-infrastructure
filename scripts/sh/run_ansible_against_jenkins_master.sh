cea19ac22852bcbdda85099733c4d3137a7f3df0
Chris O'Neil <chris.oneil@gmail.com>
2019-04-08 14:34:23 +0100

    fix/script: retrieve subnet id

--- scripts/sh/run_ansible_against_jenkins_master.sh
+++ scripts/sh/run_ansible_against_jenkins_master.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
+set -e
+
 function get_subnet_id() {
     subnet_id=$(aws ec2 describe-subnets \
         --filters \
         "Name=tag:Name,Values=jenkins_environment-private-eu-west-2a" \
-        | jq '.Subnets | .[0] | .Tags[0] | .Value' \
+        | jq '.Subnets | .[0] | .SubnetId' \
         | sed 's/\"//g')
+    echo "Retrieved subnet ID for slaves as $subnet_id"
 }
 
 function run_ansible() {
     rm -rf ~/.ansible/tmp
+    echo "Running Ansible against Jenkins master... (can be 10+ seconds before output)"
     EC2_INI_PATH=/etc/ansible/ec2.ini ansible-playbook -i environments/prod \
         --limit=jenkins_master \
         --vault-password-file=~/.ansible/vault-pass \
