d085b07ea0163ad1885ef9672ffb01bb9f14cf88
Chris O'Neil <chris.oneil@gmail.com>
2019-05-08 15:53:18 +0100

    refactor/script: use image name to determine OS
    This script wasn't really generic enough for use with the dev and prod
    environment at the same time.
    
    It's now been changed to detect the OS, using the AMI ID from the
    machine description. Waiting for SSH to come online also didn't quite
    work correctly. Previously this script was being used for the prod
    environment and it turned out just by coincidence that the machines were
    always ready, because the prod environment takes much longer to
    provision.

--- scripts/sh/update_machine.sh
+++ scripts/sh/update_machine.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+#set -e
 
 machine_name=$1
 if [[ -z "$machine_name" ]]; then
@@ -8,13 +8,19 @@ if [[ -z "$machine_name" ]]; then
     exit 1
 fi
 
-key_name=$2
-if [[ -z "$key_name" ]]; then
-    key_name="ansible_prod"
+cloud_environment=$2
+if [[ -z "$cloud_environment" ]]; then
+    echo "A value for the environment must be supplied. Valid values are 'dev' or 'prod'."
+    exit 1
+fi
+
+key_path=$3
+if [[ -z "$key_path" ]]; then
+    key_path="$HOME/.ssh/ansible_$cloud_environment"
 fi
 
 function get_machine_location() {
-    if [[ "$machine_name" == *"bastion"* ]]; then
+    if [[ "$machine_name" == *"bastion"* || "$cloud_environment" == "dev" ]]; then
         field_name="PublicDnsName"
     else
         field_name="PrivateIpAddress"
@@ -22,35 +28,56 @@ function get_machine_location() {
     location=$(aws ec2 describe-instances \
         --filters \
         "Name=tag:Name,Values=$machine_name" \
+        "Name=tag:environment,Values=$cloud_environment" \
         "Name=instance-state-name,Values=running" \
         | jq ".Reservations | .[0] | .Instances | .[0] | .$field_name" \
         | sed 's/\"//g')
 }
 
+function get_machine_image_name() {
+    local image_id
+    image_id=$(aws ec2 describe-instances \
+        --filters \
+        "Name=tag:Name,Values=$machine_name" \
+        "Name=tag:environment,Values=$cloud_environment" \
+        "Name=instance-state-name,Values=running" \
+        | jq ".Reservations | .[0] | .Instances | .[0] | .ImageId" \
+        | sed 's/\"//g')
+    image_name=$(aws ec2 describe-images --image-ids "$image_id" \
+        | jq ".Images[0] | .Name" \
+        | sed 's/\"//g')
+    echo "Machine using $image_name"
+}
+
 function wait_for_ssh() {
-    ssh -i "$HOME/.ssh/$key_name" -o StrictHostKeyChecking=no ansible@"$location" ls
+    ssh -i "$key_path" -o StrictHostKeyChecking=no ansible@"$location" ls
     rc=$?
     while [[ $rc != 0 ]]
     do
-        ssh -i "$HOME/.ssh/$key_name" -o StrictHostKeyChecking=no ansible@"$location" ls
-        rc=$?
         echo "SSH not yet available on $location, sleeping for 5 seconds before retry..."
+        sleep 5
+        ssh -i "$key_path" -o StrictHostKeyChecking=no ansible@"$location" ls
+        rc=$?
     done
     echo "SSH available on $location, now running apt-get update..."
 }
 
 function run_update() {
-    if [[ "$machine_name" == *"bastion"* ]]; then
+    if [[ "$image_name" == *"CentOS"* ]]; then
         echo "Running yum update against $machine_name"
-        ssh -i "$HOME/.ssh/$key_name" -o StrictHostKeyChecking=no ansible@"$location" sudo yum update -y
-    else
+        ssh -i "$key_path" -o StrictHostKeyChecking=no ansible@"$location" sudo yum update -y
+    elif [[ "$image_name" == *"ubuntu"* ]]; then
         echo "Running apt-get update against $machine_name"
-        ssh -i "$HOME/.ssh/$key_name" -o StrictHostKeyChecking=no ansible@"$location" sudo apt-get update -y
+        ssh -i "$key_path" -o StrictHostKeyChecking=no ansible@"$location" sudo apt-get update -y
         echo "Running Python install against $machine_name"
-        ssh -i "$HOME/.ssh/$key_name" -o StrictHostKeyChecking=no ansible@"$location" sudo apt-get install -y python
+        ssh -i "$key_path" -o StrictHostKeyChecking=no ansible@"$location" sudo apt-get install -y python
+    else
+        echo "$image_name not yet supported. Please extend this script to add support."
+        exit 1
     fi
 }
 
+get_machine_image_name
 get_machine_location
 wait_for_ssh
 run_update
