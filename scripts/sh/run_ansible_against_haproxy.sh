66df8653b9782b1e788e86c22215e3b4a300d961
Chris O'Neil <chris.oneil@gmail.com>
2019-06-10 15:31:56 +0100

    fix/env: revert special behaviour for proxy run

--- scripts/sh/run_ansible_against_haproxy.sh
+++ scripts/sh/run_ansible_against_haproxy.sh
@@ -15,40 +15,13 @@ if [[ -z "$ec2_ini_file" ]]; then
     ec2_ini_file="ec2.ini"
 fi
 
-function get_proxy_location() {
-    proxy_dns=$(aws ec2 describe-instances \
-        --filters \
-        "Name=tag:Name,Values=haproxy" \
-        "Name=tag:environment,Values=$cloud_environment" \
-        "Name=instance-state-name,Values=running" \
-        | jq '.Reservations | .[0] | .Instances | .[0] | .PublicDnsName' \
-        | sed 's/\"//g')
-}
-
-function run_ansible() {
-    rm -rf ~/.ansible/tmp
-    echo "Running Ansible against proxy instance... (can be 10+ seconds before output)"
-    if [[ "$cloud_environment" == "qa" ]]; then
-        get_proxy_location
-        EC2_INI_PATH="environments/$cloud_environment/$ec2_ini_file" \
-            ansible-playbook -i "environments/$cloud_environment" \
-            --private-key="$ansible_ssh_key" \
-            --limit=haproxy \
-            --vault-password-file=~/.ansible/vault-pass \
-            -e "cloud_environment=$cloud_environment" \
-            -e "jenkins_url=$proxy_dns" \
-            -e "wg_run_on_host=True" \
-            -u ansible ansible/proxy.yml
-    else
-        EC2_INI_PATH="environments/$cloud_environment/$ec2_ini_file" \
-            ansible-playbook -i "environments/$cloud_environment" \
-            --private-key="$ansible_ssh_key" \
-            --limit=haproxy \
-            --vault-password-file=~/.ansible/vault-pass \
-            -e "cloud_environment=$cloud_environment" \
-            -e "wg_run_on_host=True" \
-            -u ansible ansible/proxy.yml
-    fi
-}
-
-run_ansible
+rm -rf ~/.ansible/tmp
+echo "Running Ansible against proxy instance... (can be 10+ seconds before output)"
+EC2_INI_PATH="environments/$cloud_environment/$ec2_ini_file" \
+    ansible-playbook -i "environments/$cloud_environment" \
+    --private-key="$ansible_ssh_key" \
+    --limit=haproxy \
+    --vault-password-file=~/.ansible/vault-pass \
+    -e "cloud_environment=$cloud_environment" \
+    -e "wg_run_on_host=True" \
+    -u ansible ansible/proxy.yml
