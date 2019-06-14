8492f58851545ab254e41976bc135d1fdf5960b0
Chris O'Neil <chris.oneil@gmail.com>
2019-06-14 12:27:25 +0100

    feat/env: new docker slave ami
    I was still seeing the failures about no installation candidates for the
    Python package. I've added a sed command to the install script that
    changes the package sources. I'm not sure if this fixes the issue
    permanently, but it was enough to generate a box.

--- scripts/sh/install_python.sh
+++ scripts/sh/install_python.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # When installing python-pip, there are sometimes prompts for user input.
-# Declaring this variable will bypass those prompts.
+# Declaring the DEBIAN_FRONTEND variable will bypass those prompts.
+sed -Ei 's/^(.*ubuntu.* main)$/\1 universe/' /etc/apt/sources.list
 export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
 apt-get install -y python
