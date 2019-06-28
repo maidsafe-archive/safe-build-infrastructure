96739b5295c1831f99a0d41179275359029af7b8
Chris O'Neil <chris.oneil@gmail.com>
2019-06-28 15:34:32 +0100

    feat/box: new AMI ID for docker slave
    I'm still experiencing issues with the python and python-pip packages
    being available on Ubuntu Bionic. I've now modified the scripts to use a
    loop for installing them. I don't know if this actually works because
    after I added these, the first time I ran it there were no issues with
    the packages! So we will see the next time the box is generated.

--- scripts/sh/install_python.sh
+++ scripts/sh/install_python.sh
@@ -1,9 +1,38 @@
 #!/usr/bin/env bash
+set -e
 
-# When installing python-pip, there are sometimes prompts for user input.
-# Declaring the DEBIAN_FRONTEND variable will bypass those prompts.
-sed -Ei 's/^(.*ubuntu.* main)$/\1 universe/' /etc/apt/sources.list
-export DEBIAN_FRONTEND=noninteractive
-apt-get update -y
-apt-get install -y python
-apt-get install -y python-pip
+function setup() {
+    # When installing python-pip, there are sometimes prompts for user input.
+    # Declaring the DEBIAN_FRONTEND variable will bypass those prompts.
+    sed -Ei 's/^(.*ubuntu.* main)$/\1 universe/' /etc/apt/sources.list
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get update -y
+}
+
+function install_python() {
+    apt-get install -y python
+    rc=$?
+    while [[ $rc != 0 ]]
+    do
+        echo "Problem with python package availability. Trying again in 5 seconds..."
+        sleep 5
+        apt-get install -y python
+        rc=$?
+    done
+}
+
+function install_python_pip() {
+    apt-get install -y python-pip
+    rc=$?
+    while [[ $rc != 0 ]]
+    do
+        echo "Problem with python-pip package availability. Trying again in 5 seconds..."
+        sleep 5
+        apt-get install -y python-pip
+        rc=$?
+    done
+}
+
+setup
+install_python
+install_python_pip
