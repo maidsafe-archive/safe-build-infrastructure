982c7d4de7eb87087b8feef0797bd01f9f15b78b
Chris O'Neil <chris.oneil@gmail.com>
2019-07-01 18:13:15 +0100

    feat/env: new terraform definitions
    This isn't really a new environment, but more a place to put things that
    are common to all environments, for example, an S3 bucket for build
    artifacts, which is what this code does. We don't really need one
    of these buckets per environment; the same bucket and user is fine for
    all.
    
    This also uses the S3 backend for Terraform state.

---
+++ terraform/common/common.tf
@@ -0,0 +1,39 @@
+terraform {
+  backend "s3" {
+    bucket = "safe-terraform-state"
+    key = "common.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+provider "aws" {
+  region = "${var.region}"
+}
+
+data "aws_iam_user" "jenkins_build_artifacts" {
+  user_name = "${var.jenkins_build_artifacts_username}"
+}
+
+resource "aws_s3_bucket" "jenkins_build_artifacts" {
+  bucket = "${var.jenkins_build_artifacts_bucket_name}"
+}
+
+resource "aws_iam_user_policy" "jenkins_build_artifacts" {
+    name = "jenkins_build_artifacts_user_policy"
+    user = "${data.aws_iam_user.jenkins_build_artifacts.user_name}"
+    policy= <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": [
+                "arn:aws:s3:::${aws_s3_bucket.jenkins_build_artifacts.bucket}",
+                "arn:aws:s3:::${aws_s3_bucket.jenkins_build_artifacts.bucket}/*"
+            ]
+        }
+   ]
+}
+EOF
+}
