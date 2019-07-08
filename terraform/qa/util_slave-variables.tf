b8b9f09ed027311abacbb56feca38d5cf42f1dae
Chris O'Neil <chris.oneil@gmail.com>
2019-07-08 19:17:29 +0100

    fix/env: correct names for security groups

--- terraform/qa/util_slave-variables.tf
+++ terraform/qa/util_slave-variables.tf
@@ -11,11 +11,11 @@ variable "util_slave_instance_type" {
 }
 
 variable "util_slave_key_pair" {
-  default = "util_slave-staging"
+  default = "util_slave-qa"
   description = "The key pair for the util slave instance"
 }
 
 variable "util_slaves_security_group" {
-  default = "util_slaves-staging"
+  default = "util_slaves-qa"
   description = "The key pair for the util slave instance"
 }
