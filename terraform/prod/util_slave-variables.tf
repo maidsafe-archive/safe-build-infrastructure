509907e96b90f24ae781d1f36c55da7c3bfed245
Chris O'Neil <chris.oneil@gmail.com>
2019-07-09 13:20:09 +0100

    fix/env: remove static util slave
    This was added when the initial design was to have this as a static
    slave, but later on it turned out to make more sense to have this
    machine run as a dynamic slave, so this static instance is no longer
    needed.

--- terraform/prod/util_slave-variables.tf
+++ terraform/prod/util_slave-variables.tf
@@ -1,15 +1,3 @@
-variable "util_slave_ami" {
-  default = {
-    eu-west-2 = "ami-0883141bc92a74917"
-  }
-  description = "AMI for util slave (Ubuntu 18.04)"
-}
-
-variable "util_slave_instance_type" {
-  default = "t3.micro"
-  description = "Instance type for util slave machine"
-}
-
 variable "util_slave_key_pair" {
   default = "util_slave-prod"
   description = "The key pair for the util slave instance"
--- terraform/qa/util_slave-variables.tf
+++ terraform/qa/util_slave-variables.tf
@@ -1,21 +1,9 @@
-variable "util_slave_ami" {
-  default = {
-    eu-west-2 = "ami-0883141bc92a74917"
-  }
-  description = "AMI for util slave (Ubuntu 18.04)"
-}
-
-variable "util_slave_instance_type" {
-  default = "t3.micro"
-  description = "Instance type for util slave machine"
-}
-
 variable "util_slave_key_pair" {
-  default = "util_slave-qa"
+  default = "util_slave-prod"
   description = "The key pair for the util slave instance"
 }
 
 variable "util_slaves_security_group" {
-  default = "util_slaves-qa"
+  default = "util_slaves-prod"
   description = "The key pair for the util slave instance"
 }
