017859e98a08bfe27d6591498090280c92e2110f
Chris O'Neil <chris.oneil@gmail.com>
2019-07-11 11:36:25 +0100

    feat/env: new IAM user for building AMIs
    As the comment in the code says, this was taken from the Packer
    documentation. So far I've tested that the box generation can run with
    this user, but there's been a problem with the Ansible run, so I can't
    confirm whether these permissions work end to end, but I suspect they'll
    be fine.

--- terraform/common/common.tf
+++ terraform/common/common.tf
@@ -67,3 +67,58 @@ resource "aws_iam_user_policy" "jenkins_deploy_artifacts" {
 }
 EOF
 }
+
+data "aws_iam_user" "packer" {
+  user_name = "${var.packer_username}"
+}
+
+# This user policy was taken from here:
+# https://www.packer.io/docs/builders/amazon.html#using-an-iam-instance-profile
+resource "aws_iam_user_policy" "packer" {
+    name = "packer_user_policy"
+    user = "${data.aws_iam_user.packer.user_name}"
+    policy= <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [{
+      "Effect": "Allow",
+      "Action" : [
+        "ec2:AttachVolume",
+        "ec2:AuthorizeSecurityGroupIngress",
+        "ec2:CopyImage",
+        "ec2:CreateImage",
+        "ec2:CreateKeypair",
+        "ec2:CreateSecurityGroup",
+        "ec2:CreateSnapshot",
+        "ec2:CreateTags",
+        "ec2:CreateVolume",
+        "ec2:DeleteKeyPair",
+        "ec2:DeleteSecurityGroup",
+        "ec2:DeleteSnapshot",
+        "ec2:DeleteVolume",
+        "ec2:DeregisterImage",
+        "ec2:DescribeImageAttribute",
+        "ec2:DescribeImages",
+        "ec2:DescribeInstances",
+        "ec2:DescribeInstanceStatus",
+        "ec2:DescribeRegions",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeSnapshots",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeTags",
+        "ec2:DescribeVolumes",
+        "ec2:DetachVolume",
+        "ec2:GetPasswordData",
+        "ec2:ModifyImageAttribute",
+        "ec2:ModifyInstanceAttribute",
+        "ec2:ModifySnapshotAttribute",
+        "ec2:RegisterImage",
+        "ec2:RunInstances",
+        "ec2:StopInstances",
+        "ec2:TerminateInstances"
+      ],
+      "Resource" : "*"
+  }]
+}
+EOF
+}
